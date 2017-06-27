16b508ebcd897e2f8e8a0642c8b0334dc9610bdc
balegas <v.sousa@campus.fct.unl.pt>
2017-06-27 22:27:49 +0100

    Completed crawler tests

--- crawler_test.go
+++ crawler_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"os"
+	"log"
 	"testing"
 	"time"
 
@@ -14,11 +14,24 @@ func TestCrawlersMock(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	domainNames := []string{"domainGGC.com", "www.domainGGC.com"}
+	domainURLs := []string{
+		"http://www.domainGGC.com/",
+		"http://www.domainGGC.com/nonExistingPage.html",
+		"http://www.domainGGC.com/page1.html",
+		"http://www.domainGGC.com/page2.html",
+		"http://www.domainGGC.com/page3.html",
+		"http://www.domainGGC.com/source.js",
+		"http://www.domainGGC.com/theme.css",
+	}
+
+	domainNames := []string{"www.domainGGC.com"}
 	setUpFakePage("http://www.domainGGC.com/", "testFiles/home.html")
-	setUpFakePage("http://domainGGC.com/", "testFiles/home.html")
-	setUpFakePage("http://www.domainGGC.com/page1/", "testFiles/page1.html")
-	setUpFakePage("http://domainGGC.com/page1/", "testFiles/page1.html")
+	setUpFakePage("http://www.domainGGC.com/page1.html", "testFiles/page1.html")
+	setUpFakePage("http://www.domainGGC.com/page2.html", "testFiles/page2.html")
+	setUpFakePage("http://www.domainGGC.com/page3.html", "testFiles/page3.html")
+	setUpFakePage("http://www.domainGGC.com/source.js", "testFiles/source.js")
+	setUpFakePage("http://www.domainGGC.com/theme.css", "testFiles/theme.css")
+
 	oneSeconds := time.Duration(1) * time.Second
 
 	bC := NewBasicCrawlerWithDomainPolicy("GGC", domainNames, oneSeconds)
@@ -32,7 +45,21 @@ func TestCrawlersMock(t *testing.T) {
 		if error != nil {
 			t.Fail()
 		}
-		sm.printSitemap(os.Stdout)
+		links := sm.getOrderedLinks()
+
+		if len(links) != len(domainURLs) {
+			t.Errorf("Number of URLs found does not match.")
+		}
+
+		log.Printf("%v", links)
+
+		for i, l := range links {
+			if l != domainURLs[i] {
+				t.Errorf("URL different from expected: %v C: %v, E: %v", i, l, domainURLs[i])
+			}
+
+		}
+
 	}
 
 }
